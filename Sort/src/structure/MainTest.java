d988eeda381ded3d12af4f9450660713b7044ad6
yud11 <1969527445@qq.com>
2022-10-25 17:16:39 +0800

    yud1:二分查找代码更新

--- Sort/src/structure/MainTest.java
+++ Sort/src/structure/MainTest.java
@@ -9,9 +9,10 @@ public class MainTest {
     public static void main(String[] args) {
 //        new MergeSort(100).sort();
 //        new QuickSort(100).sort();
-        new ShellSort(100).sort();
-        new SelectSort(100).sort();
-        new BubbleSort(100).sort();
+//        new ShellSort(100).sort();
+//        new SelectSort(100).sort();
+//        new BubbleSort(100).sort();
+        new InsertSort(100).sort();
 
     }
 }
