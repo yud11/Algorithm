313d77f936ba38a4047366e0b27c997202308d82
yud11 <1969527445@qq.com>
2022-11-06 14:20:35 +0800

    yud1:LC-IPO和数据流的中位数

--- DP/leetcode/Fib.java
+++ DP/leetcode/Fib.java
@@ -1,4 +1,4 @@
-package DP;
+package DP.leetcode;
 
 /**
  * @author yudi
--- Sort/leetcode/SmallestK.java
+++ Sort/leetcode/SmallestK.java
@@ -9,17 +9,17 @@ import java.util.PriorityQueue;
  */
 public class SmallestK {
 
-    public int[] smallestK(int[] arr, int k) {
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
-        for (int i = 0; i < arr.length; i++) {
-            queue.add(arr[i]);
-        }
-        int[] ans = new int[k];
-        for (int i = 0; i < ans.length; i++) {
-            ans[i] =  queue.poll();
-        }
-        return ans;
-
-        Arrays.sort();
-    }
+//    public int[] smallestK(int[] arr, int k) {
+//        PriorityQueue<Integer> queue = new PriorityQueue<>();
+//        for (int i = 0; i < arr.length; i++) {
+//            queue.add(arr[i]);
+//        }
+//        int[] ans = new int[k];
+//        for (int i = 0; i < ans.length; i++) {
+//            ans[i] =  queue.poll();
+//        }
+//        return ans;
+//
+//        Arrays.sort();
+//    }
 }
